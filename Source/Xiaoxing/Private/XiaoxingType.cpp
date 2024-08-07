3e746cc4a0026e29125c73af72af64b4d3e6c3e8
cgehy <ghybaoxue@163.com>
2024-08-07 10:47:57 +0800

    Create XiaoxingType.cpp

---
+++ Source/Xiaoxing/Private/XiaoxingType.cpp
@@ -0,0 +1,22 @@
+#include "XiaoxingType.h"
+
+FXiaoxingCompletionParam::FXiaoxingCompletionParam()
+{
+	Bot = EXiaoxingBot::TEACHER_GE;
+	UserId = TEXT("tsidy9y3yjxj8byakbs9");
+}
+
+FXiaoxingCompletionResponse::FXiaoxingCompletionResponse()
+{
+
+}
+
+FXiaoxingAnswer::FXiaoxingAnswer()
+{
+
+}
+
+FXiaoxingMeta::FXiaoxingMeta()
+{
+
+}
