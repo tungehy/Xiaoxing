784c9e890bb5c7746036fc9fe15ebfdc9eb38a71
cgehy <ghybaoxue@163.com>
2024-08-07 10:47:54 +0800

    Create XiaoxingBPLibrary.cpp

---
+++ Source/Xiaoxing/Private/XiaoxingBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "XiaoxingBPLibrary.h"
+#include "Xiaoxing.h"
+
+UXiaoxingBPLibrary::UXiaoxingBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UXiaoxingBPLibrary::XiaoxingSampleFunction(float Param)
+{
+	return -1;
+}
+
